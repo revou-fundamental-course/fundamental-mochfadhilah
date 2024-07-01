6006d1701c60a6da1b7726a14a371ea88d4ec85d
mochfadhilah <moch.fadhilah1@gmail.com>
2024-07-01 20:26:43 +0700

    update tgl 01 07 2024 (v1.2)

--- js/script.js
+++ js/script.js
@@ -7,16 +7,53 @@ function btnf() {
     const pesan = document.getElementById('pesan');
     const jkel = document.getElementsByName('jkel');
     const tgl = new Date;
+    // validation
+    const enama = document.getElementById('error-name');
+    const etanggal = document.getElementById('error-tanggal');
+    const ejkel = document.getElementById('error-jkel');
+    const epesan = document.getElementById('error-pesan');
+    // input
     const itime = document.getElementById('itime');
     const inama = document.getElementsByClassName ('inama');
     const itanggal = document.getElementById('itanggal');
     const ijkel = document.getElementById('ijkel');
     const ipesan = document.getElementById('ipesan');
-    if ( nama.value == "" || tanggal.value == ""  || jkel.value == "" || pesan.value == "") {
-        alert("lengkapi data anda terlebih dahulu");
-        document.getElementById("pnama").focus;
-        return;
+    console.log(jkel);
+    if ( nama.value == "") {
+      enama.innerHTML = "masukkan nama terlebih dahulu";
+      etanggal.innerHTML = "";
+      ejkel.innerHTML = "";
+      epesan.innerHTML = "";
+      nama.focus;
+      return;
+    } else if (tanggal.value == "") {
+      enama.innerHTML = "";
+      etanggal.innerHTML = "masukkan tanggal lahir terlebih dahulu";
+      ejkel.innerHTML = "";
+      epesan.innerHTML = "";
+      tanggal.focus;
+      return;
+    } else if (document.querySelectorAll('input[type="radio"]:checked').length === 0) {
+      enama.innerHTML = "";
+      etanggal.innerHTML = "";
+      ejkel.innerHTML = "Pilih jenis kelamin anda";
+      epesan.innerHTML = "";
+      jkel.focus;
+      return;
+    } else if (pesan.value == "") {
+      enama.innerHTML = "";
+      etanggal.innerHTML = "";
+      ejkel.innerHTML = "";
+      epesan.innerHTML = "masukkan pesan terlebih dahulu";
+      pesan.focus;
+      return;
     }
+    // validasi hilang
+    enama.innerHTML ="";
+    etanggal.innerHTML ="";
+    ejkel.innerHTML ="";
+    epesan.innerHTML ="";
+    // box variabel
     itime.innerHTML = tgl;
     inama[0].innerHTML = nama.value;
     inama[1].innerHTML = nama.value;
